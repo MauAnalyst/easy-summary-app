ba97e81b93867c28d1eae427621dba8db3876f78
MauAnalyst <164758301+MauAnalyst@users.noreply.github.com>
2025-01-08 23:23:17 -0300

    feat: add zod for validation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,36 +2,60 @@ import { FastifyInstance } from "fastify";
 import { UserUseCase } from "../usecases/user.usecase";
 import { UserCreate, UserLogin } from "../interfaces/user.interface";
 import { authenticate } from "../middlewares/auth.middleware";
+import { z } from "zod";
+
+const userSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
 
 export async function userRoutes(fastify: FastifyInstance) {
   const userUseCase = new UserUseCase();
   fastify.post<{ Body: UserCreate }>("/create", async (req, reply) => {
-    const { name, email, password } = req.body;
+    //const { name, email, password } = req.body;
     try {
-      const data = await userUseCase.create({
+      const validatedData = userSchema.parse(req.body);
+      const { name, email, password } = validatedData;
+
+      const result = await userUseCase.create({
         name,
         email,
         password,
       });
 
-      return reply.send(data);
+      return reply.send(result);
     } catch (error) {
       reply.send(error);
     }
   });
 
   fastify.post<{ Body: UserLogin }>("/login", async (req, reply) => {
-    const { email, password } = req.body;
+    //const { email, password } = req.body;
 
     try {
+      const validatedData = loginSchema.parse(req.body);
+
+      const { email, password } = validatedData;
+
       const user = await userUseCase.login({
         email,
         password,
       });
-      const token = fastify.jwt.sign({ email: user.email, id: user.id });
+      const token = fastify.jwt.sign(
+        { email: user.email, id: user.id },
+        { expiresIn: "5m" }
+      );
 
       return token;
-    } catch (error) {}
+    } catch (error) {
+      reply.send(error);
+    }
   });
 
   fastify.get(
